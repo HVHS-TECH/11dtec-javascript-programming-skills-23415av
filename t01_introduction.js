d79acee74cc149944c5cf0e830eb14bcffa1f392
23415av <23415av@hvhs.school.nz>
2025-04-28 21:41:27 +0000

    Apr 28, 2025, 9:41 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,5 +1,8 @@
 /*************************
-Name of task:
+Name Of Task: Intro To Java Script
+Name Of Person: Achlys Vincent
+Date: Week One Of Term Two
+Purpose: To Begin Leaening To Write Vanilla Java Script
 *************************/
 console.log("Running t01_introduction.js");
 
