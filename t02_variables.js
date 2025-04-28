2e6a29d1e71ca64033fc3805863efd11f784fee1
23415av <23415av@hvhs.school.nz>
2025-04-28 21:57:05 +0000

    Apr 28, 2025, 9:57 PM

---
+++ t02_variables.js
@@ -0,0 +1,21 @@
+/*************************
+Name Of Task: Intro To Java Script
+Name Of Person: Achlys Vincent
+Date: Week One Of Term Two 2025
+Purpose: To Begin Learning To Write Vanilla Java Script
+*************************/
+console.log("Dandys World? I don't know.");
+
+//Variables
+
+
+/*************************
+Main Code
+*************************/
+
+
+
+
+/*************************
+Functions
+*************************/
