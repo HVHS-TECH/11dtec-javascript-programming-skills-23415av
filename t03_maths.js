ef92a56288ec913cd93eb5835d82377221836966
23415av <23415av@hvhs.school.nz>
2025-04-30 00:23:24 +0000

    math

--- t03_maths.js
+++ t03_maths.js
@@ -4,14 +4,32 @@ Name Of Person: Achlys Vincent
 Date: Week One Of Term Two 2025
 Purpose: To Start Using Math In Java Script
 *************************/
-console.log("Math");
 
 //Variables
 
+var userName = "Achlys";
+var userAge = 14;
+var pocketMoney = 5.22;
+var currentYear = 2025;
+var birthYear = 2010;
+var tenYears = 24;
+var divide = 2
 
 /*************************
 Main Code
 *************************/
+
+console.log("Hi",userName,
+"                                                 ",
+"As of",currentYear,"you are",userAge,"years old",
+"                                            ",
+"You were born in" ,birthYear,
+"                                                                ",
+"In 10 years you will be" ,tenYears, "years old",
+"                                                   ",
+"You have" ,pocketMoney, "dollars",
+"                                                         ",
+"Half of your money is" ,pocketMoney / divide,);
 
 
 
