4bfa6f38f745e391077be9228f80e8b2df7d9ef3
23415av <23415av@hvhs.school.nz>
2025-04-30 00:13:22 +0000

    Apr 30, 2025, 12:13 AM

--- t02_variables.js
+++ t02_variables.js
@@ -4,11 +4,10 @@ Name Of Person: Achlys Vincent
 Date: Week One Of Term Two 2025
 Purpose: To Begin Learning To Use Variables
 *************************/
-console.log(userName);
 
 //Variables
 
-var userName = Achlys;
+var userName = "Achlys";
 var userAge = 14;
 var pocketMoney = 5.22;
 var currentYear = 2025;
@@ -17,7 +16,11 @@ var currentYear = 2025;
 Main Code
 *************************/
 
-
+console.log("Hi",userName,
+"                                            ",
+"As of",currentYear,"you are",userAge,"years old",
+"                                            ",
+"You have",pocketMoney,"dollars");
 
 
 /*************************
