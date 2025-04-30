9a811365f013db33418b76b8702d547e8a90a6fa
23415av <23415av@hvhs.school.nz>
2025-04-30 00:15:12 +0000

    Apr 30, 2025, 12:15 AM

---
+++ t03_maths.js
@@ -0,0 +1,21 @@
+/*************************
+Name Of Task: Math Equations
+Name Of Person: Achlys Vincent
+Date: Week One Of Term Two 2025
+Purpose: To Start Using Math In Java Script
+*************************/
+console.log("Math");
+
+//Variables
+
+
+/*************************
+Main Code
+*************************/
+
+
+
+
+/*************************
+Functions
+*************************/
