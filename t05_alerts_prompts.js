ff62edeacf0abdf28ee46d1c31499c8128cb327d
23415av <23415av@hvhs.school.nz>
2025-05-02 02:54:23 +0000

    May 2, 2025, 2:54 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -29,7 +29,6 @@ console.log("You were born in" ,answer);
 answer = userAge + ageUp;
 console.log("In ",ageUp," years you will be" ,answer, "years old");
 ageDown = Number(ageDown);
-console.log(typeof(ageDown));
 answer = userAge - ageDown;
 console.log(ageDown, "years ago you were" ,answer, "years old",
 "\nYou have" ,pocketMoney, "dollars",);
