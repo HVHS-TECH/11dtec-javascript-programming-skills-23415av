e2db0b5a5b6a06ab08a26206c16fbad84e97b54b
23415av <23415av@hvhs.school.nz>
2025-05-02 02:58:51 +0000

    May 2, 2025, 2:58 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -11,11 +11,15 @@ Purpose: Use alerts to display messages. Use prompts to ask users for informatio
 alert("Welcome [Unknown_User] to my first Javascript program!");
 var userName = prompt("What is your name?");
 var userAge = prompt("Nice to meet you " + userName +"\nWhy don't you tell me how old you are.");
+userAge = Number(userAge);
 var currentYear = prompt("You're " +userAge+"? Nice age! \nI bet I can guess when you were born though. Just type in the current year.");
+currentYear = Number(currentYear);
 var ageUp = prompt("Wanna know how old you'll be in however many years you want? \nType in the amount of years then.");
+ageUp = Number(ageUp);
 var ageDown = prompt("Okay, but how about the opposite?");
+ageDown = Number(ageDown);
 var pocketMoney = prompt("Just one more thing I need from you " +userName+ "\nHow much money do you currently have?");
-var ten = 10;
+pocketMoney = Number(pocketMoney);
 var divide = 2
 var answer;
 
@@ -28,7 +32,7 @@ answer = currentYear - userAge;
 console.log("You were born in" ,answer);
 answer = userAge + ageUp;
 console.log("In ",ageUp," years you will be" ,answer, "years old");
-answer = Number(userAge) - Number(ageDown);
+answer = userAge - ageDown;
 console.log(ageDown, "years ago you were" ,answer, "years old",
 "\nYou have" ,pocketMoney, "dollars",);
 
