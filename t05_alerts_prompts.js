393195dac469f4d0cd33549a26368280fa5e5c5b
23415av <23415av@hvhs.school.nz>
2025-05-02 02:18:04 +0000

    May 2, 2025, 2:18 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -14,7 +14,7 @@ var userAge = prompt("Nice to meet you " + userName +"\nWhy don't you tell me ho
 var currentYear = prompt("You're " +userAge+"? Nice age! \nI bet I can guess when you were born though. Just type in the current year.");
 var pocketMoney = prompt("Just one more thing I need from you " +userName+ "\nHow much money do you currently have?");
 var ten = 10;
-var divide = 2
+var divide = 2;
 var answer;
 
 /*************************
@@ -26,7 +26,7 @@ console.log("Hi",userName,
 answer = currentYear - userAge
 console.log("You were born in" ,answer,);
 answer = userAge + ten
-console.log("In 10 years you will be" ,answer, "years old",
+console.log("In 10 years you will be " ,answer, "years old",
 
 "\nYou have" ,pocketMoney, "dollars",);
 
