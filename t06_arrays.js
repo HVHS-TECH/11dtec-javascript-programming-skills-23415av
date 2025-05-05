b1eaa782d3cf71d0d7e40d8848be2f20f7944228
23415av <23415av@hvhs.school.nz>
2025-05-05 21:26:43 +0000

    May 5, 2025, 9:26 PM

--- t06_arrays.js
+++ t06_arrays.js
@@ -12,11 +12,11 @@ var extraCookie = ["Espresso Cookie", "Shadow Milk Cookie", "Wizard Cookie", "Pu
 Main Code
 *************************/
 alert("Ever heard of Cookie Run Kingdom? Doesn't matter! In CRK there are multiple rarites and over one hundred cookies. \nToday, I have picked five cookies of different rarites for you to try and get.");
-alert("Our Common cookie is". cookie[2]);
-alert("Our Rare cookie is". cookie[4]);
-alert("Our Epic cookie is". cookie[0]);
-alert("Our Ancient cookie is". cookie[3]);
-alert("Our Beast cookie is". cookie[1]);
+alert("Our Common cookie is", cookie[2]);
+alert("Our Rare cookie is", cookie[4]);
+alert("Our Epic cookie is", cookie[0]);
+alert("Our Ancient cookie is", cookie[3]);
+alert("Our Beast cookie is", cookie[1]);
 var choice = prompt("Well. Now, it's time to get your cookie. Pick a number any number!\nAnd by any number I mean these five numbers.\n 0, 1, 2, 3, 4, or, 5.");
 alert("And Cookie number",choice,"is none other than",cookie[choice]);
 console.log("Why are you looking here? You got your cookie. It's",extraCookie[choice]);
