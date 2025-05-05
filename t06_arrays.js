f7b25fd6fc6154edd12019936acf8e62369c2f76
23415av <23415av@hvhs.school.nz>
2025-05-05 21:14:21 +0000

    May 5, 2025, 9:14 PM

--- t06_arrays.js
+++ t06_arrays.js
@@ -6,12 +6,18 @@ Purpose: Use arrays to store several pieces of information.
 *************************/
 
 //Variables
-var cookie = ["Espresso Cookie", "Shadow Milk Cookie", "Wizard Cookie", "Pure Vanilla Cookie", "Clover Cookie"];
+var cookie = ["Espresso Cookie\nWell done! You managed to get my second favourite cookie! \nHe's also the strongest cookie I have on my team.", "Shadow Milk Cookie\nOh Shadow Milk Cookie. Most of my friends think he's my favourite, but they're wrong, he ranks fourth.\nHe is my second strongest though, so I guess he wins.", "Wizard Cookie", "Pure Vanilla Cookie", "Clover Cookie"];
 
 /*************************
 Main Code
 *************************/
-alert("Ever heard of Cookie Run Kingdom? Doesn't matter! In CRK there are mult");
+alert("Ever heard of Cookie Run Kingdom? Doesn't matter! In CRK there are multiple rarites and over one hundred cookies. \nToday, I have picked five cookies of different rarites for you to try and get.");
+alert("Our Common cookie is". cookie[2])
+alert("Our Rare cookie is". cookie[4])
+alert("Our Epic cookie is". cookie[0])
+alert("Our Ancient cookie is". cookie[3])
+alert("Our Beast cookie is". cookie[1])
+
 /*************************
 Functions
 *************************/
