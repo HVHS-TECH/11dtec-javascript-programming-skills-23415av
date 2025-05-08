a70142bad05a73192f15912f07ae693eb25cc72a
23415av <23415av@hvhs.school.nz>
2025-05-08 01:28:19 +0000

    May 8, 2025, 1:28 AM

---
+++ t09_functions.js
@@ -0,0 +1,23 @@
+/*************************
+Name Of Task: Functions
+Name Of Person: Achlys Vincent
+Date: Week Two Of Term Two 2025
+Purpose: Define and call functions. 
+*************************/
+console.log("I am so tired as of writing this code because I rearranged my entire room yesterday.");
+
+//Variables
+
+
+/*************************
+Main Code
+*************************/
+alert ("Welcome again. No I don't remeber your name. Yes I'm going to ask again");
+function askUserName() {
+    userName = prompt("Go on");
+    alert ("Yes. Hi " + userName);
+}
+
+/*************************
+Functions
+*************************/
