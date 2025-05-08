8b9ec3b65d4d77d0195e0ab8f8f54ae50293de9c
23415av <23415av@hvhs.school.nz>
2025-05-08 00:43:36 +0000

    May 8, 2025, 12:43 AM

---
+++ t08_for_loop.js
@@ -0,0 +1,21 @@
+/*************************
+Name Of Task: Iteration - The For Loop
+Name Of Person: Achlys Vincent
+Date: Week Two Of Term Two 2025
+Purpose: Get a section of code to repeat a certain number of times.
+*************************/
+console.log("I am so tired as of writing this code because I rearranged my entire room yesterday and then played Dnadys world so I wouldn't cry.");
+
+//Variables
+
+
+/*************************
+Main Code
+*************************/
+
+
+
+
+/*************************
+Functions
+*************************/
