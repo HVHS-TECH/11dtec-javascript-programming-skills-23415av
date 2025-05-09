e868f0c90cc109b66bc33bb484b48c99083666fe
23415av <23415av@hvhs.school.nz>
2025-05-09 02:45:13 +0000

    May 9, 2025, 2:45 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -20,15 +20,6 @@ function askUserAge() {
 function askUserMoney() {
     userMoney = prompt("Money");
     userMoney = Number(userMoney);
-if (userMoney < 4){
-    console.log("Sorry " + userName + ", that is not enough for a $4 chocolate bar\nSimply get more money and go buy one for me. I'm sure " + userAge + " is a good age to work at");
-}
-else if (userMoney > 4){
-    console.log("Lookie lookie " + userName + "! You have more than enough for a $4 chocolate bar. You should buy one and give it to me\nInfact. Do it now please. All " + userAge + " year olds should");
-}
-else if (userMoney == 4){
-    console.log(+ userName + ". You have JUST enough money for a $4 chocolate bar. If you choose to buy one, you should share with me\nNot should. Do. Please. At " + userAge + " you should be always doing this");
-}
 }
 /*************************
 Main Code
@@ -38,7 +29,15 @@ userName = askUserName ();
 userAge = askUserAge ();
 userMoney = askUserMoney ();
 }
-
+if (userMoney < 4){
+    console.log("Sorry " + userName + ", that is not enough for a $4 chocolate bar\nSimply get more money and go buy one for me. I'm sure " + userAge + " is a good age to work at");
+}
+else if (userMoney > 4){
+    console.log("Lookie lookie " + userName + "! You have more than enough for a $4 chocolate bar. You should buy one and give it to me\nInfact. Do it now please. All " + userAge + " year olds should");
+}
+else if (userMoney == 4){
+    console.log(+ userName + ". You have JUST enough money for a $4 chocolate bar. If you choose to buy one, you should share with me\nNot should. Do. Please. At " + userAge + " you should be always doing this");
+}
 /*************************
 Functions
 *************************/
