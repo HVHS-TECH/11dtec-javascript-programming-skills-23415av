9ba7ccaa8ad8db89f6f3a7cb1637e440bc55561b
23415av <23415av@hvhs.school.nz>
2025-05-09 02:43:39 +0000

    May 9, 2025, 2:43 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -20,17 +20,6 @@ function askUserAge() {
 function askUserMoney() {
     userMoney = prompt("Money");
     userMoney = Number(userMoney);
-}
-/*************************
-Main Code
-*************************/
-function start () {
-userName = askUserName ();
-userAge = askUserAge ();
-userAge = Number(userAge);
-userMoney = askUserMoney ();
-userMoney = Number(userMoney);
-}
 if (userMoney < 4){
     console.log("Sorry " + userName + ", that is not enough for a $4 chocolate bar\nSimply get more money and go buy one for me. I'm sure " + userAge + " is a good age to work at");
 }
@@ -40,6 +29,16 @@ else if (userMoney > 4){
 else if (userMoney == 4){
     console.log(+ userName + ". You have JUST enough money for a $4 chocolate bar. If you choose to buy one, you should share with me\nNot should. Do. Please. At " + userAge + " you should be always doing this");
 }
+}
+/*************************
+Main Code
+*************************/
+function start () {
+userName = askUserName ();
+userAge = askUserAge ();
+userMoney = askUserMoney ();
+}
+
 /*************************
 Functions
 *************************/
