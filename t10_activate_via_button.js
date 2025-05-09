7ca2602e2bf47803c971239762a5d5e50b509dec
23415av <23415av@hvhs.school.nz>
2025-05-09 02:30:46 +0000

    May 9, 2025, 2:30 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -7,12 +7,37 @@ Purpose: Get a html button to work.
 
 
 //Variables
-
+function askUserName() {
+    userName = prompt("Go on");
+    alert("Yes. Hi " + userName);
+}
+function askUserAge() {
+    userAge = prompt("And the age was?");
+    userAge = Number(userAge);
+    alert("Only " + userAge + "?");
+}
+function askUserMoney() {
+    userMoney = prompt("How much money you have?");
+    userMoney = Number(userMoney);
+    alert("Oh wow. " + userMoney + " Dollars.");
+}
 
 /*************************
 Main Code
 *************************/
-
+alert("Welcome again. No I don't remeber your name. Yes I'm going to ask again");
+askUserName ()
+askUserAge ()
+askUserMoney ()
+if (userMoney < 4){
+    console.log("That is not enough for a $4 chocolate bar\nSimply get more money and go buy one for me");
+}
+else if (userMoney > 4){
+    console.log("Lookie lookie! You have more than enough for a $4 chocolate bar. You should buy one and give it to me\nInfact. Do it now please");
+}
+else if (userMoney == 4){
+    console.log("You have JUST enough money for a $4 chocolate bar. If you choose to buy one, you should share with me\nNot should. Do. Please.");
+}
 /*************************
 Functions
 *************************/
