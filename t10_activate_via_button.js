aa0a3783de21cfe5a3f4db1c413c64668701f44e
23415av <23415av@hvhs.school.nz>
2025-05-09 02:29:05 +0000

    May 9, 2025, 2:29 AM

---
+++ t10_activate_via_button.js
@@ -0,0 +1,18 @@
+/*************************
+Name Of Task: Activate VIA  button
+Name Of Person: Achlys Vincent
+Date: Week Two Of Term Two 2025
+Purpose: Get a html button to work.
+*************************/
+
+
+//Variables
+
+
+/*************************
+Main Code
+*************************/
+
+/*************************
+Functions
+*************************/
