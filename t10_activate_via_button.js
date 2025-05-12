4ab182e6a5ec8df96569fea19febe458271a56cf
23415av <23415av@hvhs.school.nz>
2025-05-12 21:13:31 +0000

    May 12, 2025, 9:13 PM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -22,7 +22,7 @@ function start() {
         console.log("Sorry " + userName + ", that is not enough for a $4 chocolate bar\nSimply get more money and go buy one for me. I'm sure " + userAge + " is a good age to work at");
     }
     else if (userMoney > 4) {
-        console.log("Lookie lookie " + userName + "! You have more than enough for a $4 chocolate bar. You should buy one and give it to me\nInfact. Do it now please. All " + userAge + " year olds should");
+        console.log("Lookie lookie " + userName + "! You have more than enough for a $4 chocolate bar. You should buy one and give it to me\nIn fact. Do it now please. All " + userAge + " year olds should");
     }
     else if (userMoney == 4) {
         console.log(+ userName + ". You have JUST enough money for a $4 chocolate bar. If you choose to buy one, you should share with me\nNot should. Do. Please. At " + userAge + " you should be always doing this");
