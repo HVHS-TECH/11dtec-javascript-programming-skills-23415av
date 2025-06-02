0a6b4a85930fed4577fd3b962d7af294ff20d3c5
23415av <23415av@hvhs.school.nz>
2025-06-02 21:20:59 +0000

    Jun 2, 2025, 9:20 PM

--- t13_validation.js
+++ t13_validation.js
@@ -8,52 +8,64 @@ Purpose: Only allow valid input from the user.
 
 //Variables
 let userName;
+let userNameIsInvalid = true;
 const MINAGE = 0;
 const MAXAGE = 130;
 let userAge;
 let userAgeIsInvalid = true;
 let userMoney;
+let userMoneyIsInvalid = true;
 
 /*************************
 Main Code
 *************************/
 function start() {
-    
-    if (!isNaN(userName) || userName == null || userName == " " || userName == " ") {
-        userName = ("Nice try buckaroo, but that is an invalid prompt. Numbers are not aloud. Try again.");
+    while (userNameIsInvalid) {
+        userName = prompt("Please import a valid age")
+        if (!isNaN(userName) || userName == null || userName == " " || userName == " ") {
+            alert = ("Nope. That isn't a name. Those aren't letters.");
+        } else {
+            userNameIsInvalid = false;
+        }
+    }
+    while (userAgeIsInvalid) {
+        userAge = prompt("Please import a valid age")
+        if (userAge == null || userAge == " " || userAge == " " || isNaN(userAge)) {
+            alert = ("Nope. Numbers only, or it isn't valid. It also has to be between " + MINAGE + " and " + MAXAGE + ", or I don't believe you.");
+        } else {
+            userAgeIsInvalid = false;
+        }
+    }
+    while (userMoneyIsInvalid) {
+        userMoney = prompt("Please import a valid age")
+        if (userMoney == null || userMoney == " " || userMoney == " " || isNaN(userMoney)) {
+            alert = ("Nope. Numbers only, or it isn't valid.");
+        } else {
+            userMoneyIsInvalid = false;
+        }
     }
 
-    userMoney = askUserMoney();
-    if (userMoney == null || userMoney == " " || userMoney == " " || isNaN(userMoney)) {
-        userMoney = ("Nope. Numbers only, or it isn't valid.");
-    }
     if (userMoney = 5) {
-        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nLindt Block - $5"
+        alert = "What you, a " + userAge + " year old named " + userName + ", can afford:\nLindt Block - $5"
     } else if (userMoney = 4) {
-        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nDairy Milk Block - $4"
+        alert = "What you, a " + userAge + " year old named " + userName + ", can afford:\nDairy Milk Block - $4"
     } else if (userMoney = 3) {
-        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nKing Size Crunchy - $3"
+        alert = "What you, a " + userAge + " year old named " + userName + ", can afford:\nKing Size Crunchy - $3"
     } else if (userMoney = 2) {
-        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nMoro bar - $2"
+        alert = "What you, a " + userAge + " year old named " + userName + ", can afford:\nMoro bar - $2"
     } else if (userMoney = 1) {
-        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nMars bar - $1"
+        alert = "What you, a " + userAge + " year old named " + userName + ", can afford:\nMars bar - $1"
     } else if (userMoney = 0) {
-        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nNothing. Oh No."
+        alert = "What you, a " + userAge + " year old named " + userName + ", can afford:\nNothing. Oh No."
     } else if (userMoney > 5) {
-        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nLindt Block - $5"
-    }
-
-    /*************************
-    Functions
-    *************************/
-
-
-
-while (userAgeIsInvalid) {
-    userAge = prompt ("Please import a valid age")
-    if (userAge == null || userAge == " " || userAge == " " || isNaN(userAge)) {
-        alert = ("Nope. Numbers only, or it isn't valid. It also has to be between " +MINAGE+ " and " +MAXAGE+ ", or I don't believe you.");
-    } else {
-        userAgeIsInvalid = false;
+        alert = "What you, a " + userAge + " year old named " + userName + ", can afford:\nLindt Block - $5"
     }
 }
+
+/*************************
+Functions
+*************************/
+
+
+
+
