136ffdb65d9da448909131302c2e8065f18e906a
23415av <23415av@hvhs.school.nz>
2025-06-02 21:07:59 +0000

    Jun 2, 2025, 9:07 PM

--- t13_validation.js
+++ t13_validation.js
@@ -16,22 +16,31 @@ Main Code
 *************************/
 function start() {
     userName = askUserName();
+    if (!isNaN(userName) || userName == null || userName == " " || userName == " ") {
+        userName = ("Nice try buckaroo, but that is an invalid prompt. Numbers are not aloud. Try again.");
+    }
     userAge = askUserAge();
+    if (userAge == null || userAge == " " || userAge == " " || isNaN(userAge)) {
+        userAge = ("Nope. Numbers only, or it isn't valid.");
+    }
     userMoney = askUserMoney();
+    if (userMoney == null || userMoney == " " || userMoney == " " || isNaN(userMoney)) {
+        userMoney = ("Nope. Numbers only, or it isn't valid.");
+    }
     if (userMoney = 5) {
-        alert = "What you can afford:\nLindt Block - $5"
+        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nLindt Block - $5"
     } else if (userMoney = 4) {
-        alert = "What you can afford:\nDairy Milk Block - $4"
+        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nDairy Milk Block - $4"
     } else if (userMoney = 3) {
-        alert = "What you can afford:\nKing Size Crunchy - $3"
+        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nKing Size Crunchy - $3"
     } else if (userMoney = 2) {
-        alert = "What you can afford:\nMoro bar - $2"
+        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nMoro bar - $2"
     } else if (userMoney = 1) {
-        alert = "What you can afford:\nMars bar - $1"
+        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nMars bar - $1"
     } else if (userMoney = 0) {
-        alert = "What you can afford:\nNothing. Oh No."
+        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nNothing. Oh No."
     } else if (userMoney > 5) {
-        alert = "What you can afford:\nLindt Block - $5"
+        alert = "What you, a " +userAge + " year old named " +userName+ ", can afford:\nLindt Block - $5"
     }
 
     /*************************
