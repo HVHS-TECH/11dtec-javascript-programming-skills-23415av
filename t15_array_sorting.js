a45e877b92c71184de4dd8e1f2dff11b99d416d9
23415av <23415av@hvhs.school.nz>
2025-06-04 00:14:34 +0000

    Jun 4, 2025, 12:14 AM

--- t15_array_sorting.js
+++ t15_array_sorting.js
@@ -69,7 +69,7 @@ function start() {
       } if(userMoney < 1) {
             alert("You are unable to afford any chocolate bar.");
             break;
-    } if(userMoney >= chocolatePrices[i]) {
+      } if(userMoney >= chocolatePrices[i]) {
             alert("The most expensive chocolate bar you can afford is the "+chocolateBars[i]+" for $" +chocolatePrices[i]);
             break;
 }
